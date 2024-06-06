e019da72037c68c1f666e5c73e271754c4715cf1
kaiden-ong <kaiden.ong000@gmail.com>
2024-06-06 00:54:35 -0700

    Add page between home and dups

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Mirror Image
 
-## Looking to clean up your photo collection? Mirror Image helps you find and manage duplicate photos quickly and easily!
+### Looking to clean up your photo collection? Mirror Image helps you find and manage duplicate photos quickly and easily!
 
-[Download Mirror Image](mirror-image.zip)
+[Download Mirror Image Here](<Mirror Image Setup 1.0.0.exe>)
+
+### Planned & Upcoming Features:
+- Choose specific images to search for duplicates
+- Show all images on one page to easily choose which ones to delete
+- Select different kinds of files
+- Continue to improve UI/UX
+- Backend??? Accounts??? Etc.
--- public/js/render.js
+++ public/js/render.js
@@ -11,6 +11,10 @@ const findBtn = document.getElementById('findButton');
 const imgFileTypes = ['.jpeg', '.jpg', '.png', '.gif', '.bmp', '.tiff', '.tif', '.svg', '.webp',
     '.heif', '.heic', '.cr2', '.nef', '.arw', '.orf', '.rw2', '.psd', '.ico', '.eps', '.pdf', '.ai', '.kra'];
 let imgMap = {};
+let totalImg = 0;
+const title = document.getElementById("home-title");
+const subtitle = document.getElementById("home-subtitle");
+const loadingSpinner = document.getElementById("loadingSpinner");
 
 setDirBtn.addEventListener('click', () => {
 	ipcRenderer.send('select-dirs');
@@ -19,19 +23,44 @@ setDirBtn.addEventListener('click', () => {
 ipcRenderer.on('selected-directory', (event, paths) => {
     selectedDirElement.innerHTML = `<p>${paths}</p>`;
     boxContainer.style.display = 'flex';
+    imgMap = {};
+    // displayDuplicates();
+    // hideNumDup();
 });
 
 findBtn.addEventListener('click', async () => {
     console.log("clicked")
 	imgMap = {}
     const dirToSearch = selectedDirElement.textContent.trim();
+    loading();
     await Walk.walk(dirToSearch, walkFunc).then(function () {
         console.log("Done");
-      });
+    });
     // findDuplicates(dirToSearch)
-	displayNumDuplicates();
-    displayDuplicates();
+    const duplicates = Object.values(imgMap).filter(paths => paths.length > 1);
+    localStorage.setItem('dups', JSON.stringify(duplicates));
+    document.getElementById("loadingText").innerText = "Check complete!";
+    document.getElementById("loadingText").style.color = "green";
+    document.querySelector(".spinner").style.display = 'none';
+    document.querySelector(".checkmark").style.display = 'flex';
+    await delay(2000);
+    window.location.href = 'public/components/duplicate-img.html';
 });
+
+async function loading() {
+    const btnContainer = document.querySelector(".button-container");
+    const boxContainer = document.querySelector(".box-container");
+    subtitle.innerText = "";
+    btnContainer.style.display = 'none';
+    boxContainer.style.display = 'none';
+    loadingSpinner.style.display = 'flex';
+}
+
+function delay(milliseconds){
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+}
 
 async function walkFunc(err, pathname, dirent) {
     if (err) {
@@ -41,6 +70,8 @@ async function walkFunc(err, pathname, dirent) {
         console.log(pathname)
         const ext = path.extname(pathname).toLowerCase();
         if (imgFileTypes.includes(ext) && fileLocal(pathname)) {
+            totalImg++;
+            title.innerText = `Images checked: ${totalImg}\nDuplicates found: ${(Object.values(imgMap).filter(paths => paths.length > 1)).length}`;
             const hash = getFileHash(pathname);
             if (hash) {
                 if (imgMap[hash]) {
@@ -92,16 +123,6 @@ function fileLocal(filePath) {
     }
 }
 
-function isDir(path) {
-    try {
-        const stat = fs.lstatSync(path);
-        return stat.isDirectory();
-    } catch (e) {
-        console.error(`Error reading path: ${path}`, e);
-        return false;
-    }
-}
-
 function getFileHash(filePath) {
 	try {
 		const fileBuffer = fs.readFileSync(filePath);
@@ -113,48 +134,50 @@ function getFileHash(filePath) {
 	}	
 }
 
-function displayDuplicates() {
-    const duplicates = Object.values(imgMap).filter(paths => paths.length > 1);
-	console.log(duplicates);
-    const dupCardsContainer = document.querySelector('.dup-cards');
-    dupCardsContainer.innerHTML = '';
+// TODO: Use this code to create a page that shows all at once, so that we can delete more quickly
 
-    duplicates.forEach(paths => {
-        const card = document.createElement('div');
-        card.classList.add('dup-card');
-		card.style.border = '1px solid #ccc';
+// function displayDuplicates() {
+//     const duplicates = Object.values(imgMap).filter(paths => paths.length > 1);
+// 	console.log(duplicates);
+//     const dupCardsContainer = document.querySelector('.dup-cards');
+//     dupCardsContainer.innerHTML = '';
 
-		paths.forEach(filePath => {
-			const imgPath = document.createElement('p');
-            imgPath.textContent = filePath;
-			imgPath.className = "img-paths";
-			imgPath.addEventListener('click', () => openPath(filePath));
-            card.appendChild(imgPath);
-        });
+//     duplicates.forEach(paths => {
+//         const card = document.createElement('div');
+//         card.classList.add('dup-card');
+// 		card.style.border = '1px solid #ccc';
 
-		const viewButton = document.createElement('button');
-        viewButton.textContent = 'View Image';
-        viewButton.className = 'view-button';
-		viewButton.addEventListener('click', () => showImage(paths[0]));
-        card.appendChild(viewButton);
+// 		paths.forEach(filePath => {
+// 			const imgPath = document.createElement('p');
+//             imgPath.textContent = filePath;
+// 			imgPath.className = "img-paths";
+// 			imgPath.addEventListener('click', () => openPath(filePath));
+//             card.appendChild(imgPath);
+//         });
 
-        dupCardsContainer.appendChild(card);
-    });
-}
+// 		const viewButton = document.createElement('button');
+//         viewButton.textContent = 'View Image';
+//         viewButton.className = 'view-button';
+// 		viewButton.addEventListener('click', () => showImage(paths[0]));
+//         card.appendChild(viewButton);
 
-function displayNumDuplicates() {
-	const numDupsDiv = document.querySelector(".num-dups");
-	const duplicates = Object.values(imgMap).filter(paths => paths.length > 1);
-	const numDups = duplicates.length
-	numDupsDiv.innerText = "Sets of duplicates images remaining: " + numDups
-}
+//         dupCardsContainer.appendChild(card);
+//     });
+// }
 
-function showImage(filePath) {
-    console.log("Showing image:", filePath);
-    window.open(filePath);
-}
+// function displayNumDuplicates() {
+//     const numDupsDiv = document.querySelector(".num-dups");
+// 	const duplicates = Object.values(imgMap).filter(paths => paths.length > 1);
+// 	const numDups = duplicates.length
+// 	numDupsDiv.innerText = "Sets of duplicates images remaining: " + numDups
+// }
 
-function openPath(filePath) {
-	console.log("Opening image path:", filePath);
-	shell.showItemInFolder(filePath); 
-}
+// function showImage(filePath) {
+//     console.log("Showing image:", filePath);
+//     window.open(filePath);
+// }
+
+// function openPath(filePath) {
+// 	console.log("Opening image path:", filePath);
+// 	shell.showItemInFolder(filePath); 
+// }
